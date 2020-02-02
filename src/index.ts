e6f087a9505ca0f62f7fda987cdaf6320da9576a
czewail <chanzewail@gmail.com>
2020-02-02 16:37:14 +0800

    feat: export js-to-java

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import * as decorators from './decorators';
+import js2java from 'js-to-java';
 
 export * from './registry';
 export * from './dubbo-service-provider';
 export * from './base';
 export const dubbo = decorators;
+export const java = js2java;
